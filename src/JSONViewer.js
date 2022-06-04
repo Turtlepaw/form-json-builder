eb32dcb86216593577599002fd7720e2df6c9242
Antouto <anthonydsmart@gmail.com>
2022-06-04 13:42:15 +0200

    Update JSONViewer.js

--- src/JSONViewer.js
+++ src/JSONViewer.js
@@ -1,31 +1,30 @@
 import React from "react"
-import { Box, Button, Collapse, HStack, Text, Tooltip } from '@chakra-ui/react'
+import { Box, Button, Collapse, HStack, Text, Tooltip, useColorMode } from '@chakra-ui/react'
 import { MdOutlineFileDownload } from 'react-icons/md'
 
 function JSONViewer({ children }) {
   const [show, setShow] = React.useState(false)
-
+  const { colorMode } = useColorMode();
   const handleToggle = () => setShow(!show)
 
   return (
-    <Box style={{ background: '#2f3136', padding: '8px 8px 4px 8px', width: 'calc(100vw - 24px)' }}>
-      <Collapse startingHeight={147} style={{ overflow: 'scroll', bg: '#2f3136', borderRadius: '4px' }} in={show}>
+    <Box style={{ border: `1px solid ${colorMode === 'dark' ? '#e3e5e8' : '#292b2f'}`, borderRadius: '4px', background: colorMode === 'dark' ? '#f2f3f5' : '#2f3136', color: colorMode === 'dark' ? '#4f5660' : '#b9bbbe', width: 'calc(100vw - 24px)' }}>
+      <Collapse startingHeight={147} style={{ overflow: 'scroll', bg: '#2f3136', borderRadius: '4px', padding: '8px' }} in={show}>
         <pre>
           <code>{children}</code>
         </pre>
       </Collapse>
-      <Box marginBottom='4px'><hr /></Box>
-      <Box display='flex' alignItems='center' justifyContent='space-between' color='#b9bbbe'>
+      <Box display='flex' alignItems='center' justifyContent='space-between' color={colorMode === 'dark' ? '#4f5660' : '#b9bbbe'} padding='4px 12px 4px 4px' borderTop={`1px solid ${colorMode === 'dark' ? '#e3e5e8' : '#292b2f'}`}>
         <Box display='flex' alignItems='center' onClick={handleToggle}>
           <Tooltip hasArrow label={
             <Box>
               {show ? 'Collapse' : 'Expand'} ({children.split(/\r\n|\r|\n/).length} Lines)
             </Box>
-          } placement='top' shouldWrapChildren bg="#18191c" borderRadius={6}>
+          } placement='top' shouldWrapChildren bg="#18191c" borderRadius={6} >
             <svg style={{ margin: '8px', transition: 'transform 0.2s', transform: `rotate(${(show ? 0 : 180)}deg)` }} width="16" height="16" viewBox="0 0 16 16" fill="none">
               <path
                 d="M12 10L8 6L4 10"
-                stroke="#bcbcbc"
+                stroke={colorMode === 'dark' ? '#4f5660' : '#b9bbbe'}
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"

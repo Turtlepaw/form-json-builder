759de283123d35f2158a0107add8fcd289c1949c
Antouto <anthonydsmart@gmail.com>
2022-07-17 19:33:22 +0200

    Fix bug in message preview

--- src/FormPreview.js
+++ src/FormPreview.js
@@ -10,28 +10,34 @@ function FormPreview({ message, forms, displayForm, setDisplayForm }) {
 
     return (
         <Box overflowY='scroll' maxHeight='92.3vh'>
-            <Box pl='66px'>
-                <Image src='https://cdn.discordapp.com/attachments/944646735643410482/946845836879462420/forms.png' position='absolute' width='40px' clipPath='circle(50%)' left='calc(50% + 16px)' mt='5px' />
-                <Box display='flex' alignItems='center'>
-                    <Text fontFamily='Whitney Bold' _hover={{ textDecoration: 'underline' }}>Forms</Text>
-                    <Box display='flex' backgroundColor='#5865F2' borderRadius='.1875rem' ml='4px' height='.9375rem' width='39px'>
-                        <Tooltip hasArrow label={
-                            <Box>
-                                Verified Bot
+            <Box>
+                <Box display='flex' >
+                    <Image src='https://cdn.discordapp.com/attachments/944646735643410482/946845836879462420/forms.png' width='40px' height='40px' clipPath='circle(50%)' mt='5px' mr='16px' />
+                    <Box>
+                        <Box display='flex' alignItems='center'>
+                            <Text fontFamily='Whitney Bold' _hover={{ textDecoration: 'underline' }}>Forms</Text>
+                            <Box display='flex' backgroundColor='#5865F2' borderRadius='.1875rem' ml='4px' height='.9375rem' width='39px'>
+                                <Tooltip hasArrow label={
+                                    <Box>
+                                        Verified Bot
+                                    </Box>
+                                } placement='top' shouldWrapChildren bg="#18191c" borderRadius={6} padding='6px 12px'>
+                                    <svg width="16" height="16" viewBox="0 0 16 15.2"><path d="M7.4,11.17,4,8.62,5,7.26l2,1.53L10.64,4l1.36,1Z" fill="currentColor"></path></svg>
+                                </Tooltip>
+                                <Text fontFamily='Whitney Bold' fontSize='.625rem'>BOT</Text>
                             </Box>
-                        } placement='top' shouldWrapChildren bg="#18191c" borderRadius={6} padding='6px 12px'>
-                            <svg width="16" height="16" viewBox="0 0 16 15.2"><path d="M7.4,11.17,4,8.62,5,7.26l2,1.53L10.64,4l1.36,1Z" fill="currentColor"></path></svg>
-                        </Tooltip>
-                        <Text fontFamily='Whitney Bold' fontSize='.625rem'>BOT</Text>
+                            <Text fontFamily='Whitney Bold' fontSize='0.75rem' color='#a3a6aa' ml='.5rem' alignSelf='flex-end' mb='1px'>Today at {new Date().getHours()}:{new Date().getMinutes()}</Text>
+
+                        </Box>
+                        <Box>
+                            <Text><pre style={{ fontFamily: 'Whitney' }}>{message.content || ' '}</pre></Text>
+                            <Box p='4px 0'>
+                                {forms.map((form, index) => (<Button key={Math.random()} onClick={() => setDisplayForm(index)} m='4px 8px 4px 0' variant={form.button.style == 1 ? 'primary' : (form.button.style == 2 ? 'secondary' : (form.button.style == 3 ? 'success' : 'danger'))}>{form.button.label}</Button>))}
+                            </Box>
+                        </Box>
                     </Box>
-                    <Text fontFamily='Whitney Bold' fontSize='0.75rem' color='#a3a6aa' ml='.5rem' alignSelf='flex-end' mb='1px'>Today at {new Date().getHours()}:{new Date().getMinutes()}</Text>
-                    
                 </Box>
 
-                <Text><pre style={{ fontFamily: 'Whitney' }}>{message.content || ' '}</pre></Text>
-                <Box p='4px 0'>
-                    {forms.map((form, index) => (<Button key={Math.random()} onClick={() => setDisplayForm(index)} m='4px 8px 4px 0' variant={form.button.style == 1 ? 'primary' : (form.button.style == 2 ? 'secondary' : (form.button.style == 3 ? 'success' : 'danger'))}>{form.button.label}</Button>))}
-                </Box>
 
             </Box>
             {/* <Box display='flex' alignItems='center' justifyContent='space-between' m='8px'>
@@ -40,7 +46,7 @@ function FormPreview({ message, forms, displayForm, setDisplayForm }) {
                 <Button disabled={displayForm > forms.length - 2} onClick={() => setDisplayForm(displayForm + 1)}><HiChevronRight /></Button>
 
             </Box> */}
-            <Box display='flex' justifyContent='center' mt='30px'>
+            <Box display='flex' mt='30px'>
                 <Box border={`1px solid ${colorMode === 'dark' ? '#e3e5e8' : '#292b2f'}`} borderRadius='3px' width='440px' height='fit-content' maxHeight='720px' overflowY='scroll'>
                     <Box display='flex' height='fit-content' justifyContent='space-between' alignItems='center' p='16px'>
                         <Box display='flex' alignItems='center' height='24px'>

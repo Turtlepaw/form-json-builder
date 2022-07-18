7ac2a424cd34c42ece0a4527a1c187d5c886f42c
Antouto <anthonydsmart@gmail.com>
2022-07-18 09:49:49 +0200

    analytics if it works

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ root.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode='dark'/>
     <App />
+    <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "ce97e3321c604868bddcc079533f6464"}'></script>
   </React.StrictMode>
 );

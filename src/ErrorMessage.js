bf1f66a78d1fb6f6580c0c73b0a68ad97122ecff
Antouto <anthonydsmart@gmail.com>
2023-01-12 17:58:11 +0100

    Revert "📑 Embeds!"

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,20 +1,14 @@
-/* eslint eqeqeq: 0 */
 import React from 'react'
 import { HStack, Text } from '@chakra-ui/react'
 import { AiFillExclamationCircle } from 'react-icons/ai';
 
-function ErrorMessage({ children, field, fieldName = "field", fieldType = "This" }) {
-  if (field != null) children = (<>
-    {field?.type == "required" && `${fieldType} ${fieldName} is required`}
-    {field?.type == "maxLength" && `${fieldType} ${fieldName} is too long`}
-    {field?.type == "minLength" && `${fieldType} ${fieldName} is not long enough`}
-  </>);
+
+function ErrorMessage({ children }) {
+
   return (
     <HStack>
       {children ? <AiFillExclamationCircle color='#ff7a6b' /> : null}
-      <Text color='#ff7a6b'>
-        {children}
-      </Text>
+      <Text color='#ff7a6b'>{children}</Text>
     </HStack>
   )
 }

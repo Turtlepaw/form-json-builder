d664b630ab2aabe209c25f062a8c6eaa567538a8
Antouto <anthonydsmart@gmail.com>
2023-01-20 14:30:47 +0100

    Clean up stuff

--- src/App.js
+++ src/App.js
@@ -160,89 +160,78 @@ function App() {
 
   return (
     <>
-      <Grid gridTemplateRows='48px 1fr'>
-        <header>
-          <Box display='flex' alignItems='center'>
-            <img src="https://cdn.discordapp.com/attachments/944646735643410482/953304477102915624/unknown.png" alt="Forms Logo" width="28px" style={{ clipPath: 'circle(50%)' }} />
-            <nav>
-              <a href="https://discord.gg/cajZ7Mvzbp" target="_blank" rel="noopener noreferrer">Support Server</a>
-              <a href="https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D942858850850205717%26permissions%3D3072%26scope%3Dapplications.commands%2520bot" target="_blank" rel="noopener noreferrer">Invite Bot</a>
-              <a href="https://gist.github.com/Antouto/8ab83d83482af7c516f0b2b42eaee940" target="_blank" rel="noopener noreferrer">Documentation</a>
-            </nav>
-          </Box>
-          <ColorModeSwitcher height='0px' _focus={{ boxShadow: 'unset' }} />
-        </header>
-        <Grid paddingBottom={0} gridTemplateColumns='1fr 1fr'>
-          <VStack spacing={3} alignItems='flex-start' overflowY='scroll' p='16px' maxHeight='calc(100vh - 48px);'>
-            <HStack>
-              <Input
-                id="json"
-                type="file"
-                accept=".json"
-                style={{ display: 'none' }}
-                onChange={ReadFile}
-                ref={(input) => setFileInput(input)}
+      <header>
+        <Box display='flex' alignItems='center'>
+          <img src="https://cdn.discordapp.com/attachments/944646735643410482/953304477102915624/unknown.png" alt="Forms Logo" width="28px" style={{ clipPath: 'circle(50%)' }} />
+          <nav>
+            <a href="https://discord.gg/cajZ7Mvzbp" target="_blank" rel="noopener noreferrer">Support Server</a>
+            <a href="https://discord.com/login?redirect_to=%2Foauth2%2Fauthorize%3Fclient_id%3D942858850850205717%26permissions%3D3072%26scope%3Dapplications.commands%2520bot" target="_blank" rel="noopener noreferrer">Invite Bot</a>
+            <a href="https://gist.github.com/Antouto/8ab83d83482af7c516f0b2b42eaee940" target="_blank" rel="noopener noreferrer">Documentation</a>
+          </nav>
+        </Box>
+        <ColorModeSwitcher height='0px' />
+      </header>
+      <Grid paddingBottom={0} gridTemplateColumns='1fr 1fr'>
+        <VStack alignItems='flex-start' overflowY='scroll' p='16px' maxHeight='calc(100vh - 48px);'>
+          <HStack>
+            <Button onClick={() => fileInput.click()} variant="primary">Upload JSON</Button>
+            <Input id="json" type="file" accept=".json" display="none" onChange={ReadFile} ref={(input) => setFileInput(input)} />
+            <Button onClick={() => reset(ClearedValues)}>Clear All</Button>
+          </HStack>
+          <Box width='100%'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <MessageBuilder
+                {...{ Defaults, errors, messageType, register, setMessageType, setValue }}
               />
-              <FormLabel htmlFor="json" style={{ cursor: 'pointer' }}>
-                <Button onClick={() => fileInput.click()} variant="primary">Upload JSON</Button>
-              </FormLabel>
-              <Button onClick={() => reset(ClearedValues)}>Clear All</Button>
-            </HStack>
-            <Box width='100%'>
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <MessageBuilder
-                  {...{ Defaults, errors, messageType, register, setMessageType, setValue }}
-                />
 
-                <FormLabel pb={2}>Forms</FormLabel>
-                <FieldArray
-                  {...{ control, register, defaultValues, getValues, setValue, formState, watch, displayForm, setDisplayForm }}
-                />
-              </form>
-            </Box>
-            <VStack width='100%' align='flex-start'>
-              <Heading size='sm' marginBottom='5px'>Form Configuration File</Heading>
-              <Text>
-                This is the configuration file you'll need to give to the <UserMention>Forms</UserMention> bot to create your form. The <UserMention>Forms</UserMention> bot needs to be in your server.
-              </Text>
-              <JSONViewer>{JSON.stringify(watch(), null, 2)}</JSONViewer>
-              <VStack alignItems='flex-start'>
-                <HStack alignItems='flex-start'>
-                  <Button
-                    variant='success'
-                    disabled={!formState.isValid}
-                    onClick={() => {
-                      const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
-                        JSON.stringify(watch(), null, 2)
-                      )}`;
-                      const link = document.createElement("a");
-                      link.href = jsonString;
-                      link.download = 'form.json';
-                      link.click();
-                    }}
-                  >
-                    Download Configuration File
-                  </Button>
-                  <Button onClick={fixForm}>Fix Form</Button>
-                </HStack>
-                {!formState.isValid && <ErrorMessage>Fill out the fields correctly before downloading the configuration file.</ErrorMessage>}
-              </VStack>
-              <Text maxWidth={450}>
-                Upload the configuration file using the <SlashCommand>form create</SlashCommand> command on the <UserMention>Forms</UserMention> bot.
-              </Text>
+              <FormLabel pb={2}>Forms</FormLabel>
+              <FieldArray
+                {...{ control, register, defaultValues, getValues, setValue, formState, watch, displayForm, setDisplayForm }}
+              />
+            </form>
+          </Box>
+          <VStack width='100%' align='flex-start'>
+            <Heading size='sm' marginBottom='5px'>Form Configuration File</Heading>
+            <Text>
+              This is the configuration file you'll need to give to the <UserMention>Forms</UserMention> bot to create your form. The <UserMention>Forms</UserMention> bot needs to be in your server.
+            </Text>
+            <JSONViewer>{JSON.stringify(watch(), null, 2)}</JSONViewer>
+            <VStack alignItems='flex-start'>
+              <HStack alignItems='flex-start'>
+                <Button
+                  variant='success'
+                  disabled={!formState.isValid}
+                  onClick={() => {
+                    const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+                      JSON.stringify(watch(), null, 2)
+                    )}`;
+                    const link = document.createElement("a");
+                    link.href = jsonString;
+                    link.download = 'form.json';
+                    link.click();
+                  }}
+                >
+                  Download Configuration File
+                </Button>
+                <Button onClick={fixForm}>Fix Form</Button>
+              </HStack>
+              {!formState.isValid && <ErrorMessage>Fill out the fields correctly before downloading the configuration file.</ErrorMessage>}
             </VStack>
-            <Box className="text-sm pt-5">
-              <p className="font-medium">©️ 2023 Forms Discord Bot</p>
-              <p className="text-muted">
-                Made with <svg style={{ display: "inline-block", marginLeft: "1px", marginRight: "1px" }} width={15} height={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path fill="#DD2E44" d="M35.885 11.833c0-5.45-4.418-9.868-9.867-9.868-3.308 0-6.227 1.633-8.018 4.129-1.791-2.496-4.71-4.129-8.017-4.129-5.45 0-9.868 4.417-9.868 9.868 0 .772.098 1.52.266 2.241C1.751 22.587 11.216 31.568 18 34.034c6.783-2.466 16.249-11.447 17.617-19.959.17-.721.268-1.469.268-2.242z" /></svg>
-                {' '}from <UserMention text="#d0d3d8">Anthony</UserMention> and <UserMention text="#d0d3d8">Turtlepaw</UserMention>
-                <br />
-                This website is <Link href='https://github.com/Antouto/form-builder'>open-source</Link>
-              </p>
-            </Box>
+            <Text maxWidth={450}>
+              Upload the configuration file using the <SlashCommand>form create</SlashCommand> command on the <UserMention>Forms</UserMention> bot.
+            </Text>
           </VStack>
-          <FormPreview type={messageType} message={watch('message')} forms={watch('forms')} displayForm={displayForm} setDisplayForm={setDisplayForm} />
-        </Grid>
+          <Box className="text-sm pt-5">
+            <p className="font-medium">©️ 2023 Forms Discord Bot</p>
+            <p className="text-muted">
+              Made with <svg style={{ display: "inline-block", marginLeft: "1px", marginRight: "1px" }} width={15} height={15} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36"><path fill="#DD2E44" d="M35.885 11.833c0-5.45-4.418-9.868-9.867-9.868-3.308 0-6.227 1.633-8.018 4.129-1.791-2.496-4.71-4.129-8.017-4.129-5.45 0-9.868 4.417-9.868 9.868 0 .772.098 1.52.266 2.241C1.751 22.587 11.216 31.568 18 34.034c6.783-2.466 16.249-11.447 17.617-19.959.17-.721.268-1.469.268-2.242z" /></svg>
+              {' '}from <UserMention text="#d0d3d8">Anthony</UserMention> and <UserMention text="#d0d3d8">Turtlepaw</UserMention>
+              <br />
+              This website is <Link href='https://github.com/Antouto/form-builder'>open-source</Link>
+            </p>
+          </Box>
+        </VStack>
+        <FormPreview type={messageType} message={watch('message')} forms={watch('forms')} displayForm={displayForm} setDisplayForm={setDisplayForm} />
       </Grid>
     </>
   );

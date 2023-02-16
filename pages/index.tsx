9341d5d408166c41b1c42234a41ea526ff8f57bc
Antouto <anthonydsmart@gmail.com>
2023-02-16 09:22:40 +0100

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -319,7 +319,7 @@ export default function App() {
                   width={225}
                 // bgColor={loading ? "#215b32" : undefined}
                 >
-                  {!loading && "Download Configuration File"}
+                  {!loading && "Download Configuration File" + formState.isValid}
                   {(loading && SettingsModal.settings.LimitAnimations == false) && <Spinner size="sm" />}
                   {(loading && SettingsModal.settings.LimitAnimations == true) && "Downloading..."}
                 </Button>

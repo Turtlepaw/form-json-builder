510d9f41a84c9b1e7dd19ce2d7f2eda4b99efb60
Turtlepaw <hi@trtle.xyz>
2023-08-01 10:58:36 -0400

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 
+console.log(process.env.CLIENT_ID as string, process.env.CLIENT_SECRET as string)
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [

e15975c81052dc456cf6e7e54813955b639ae26e
Antouto <anthonydsmart@gmail.com>
2023-08-15 19:56:48 +0200

    Mark uppercase slash command names as invalid

--- components/messageBuilder.tsx
+++ components/messageBuilder.tsx
@@ -87,7 +87,7 @@ export default function useMessageBuilder({
                     <Text _after={{ content: '" *"', color: (colorMode === 'dark' ? '#ff7a6b' : '#d92f2f') }}>Name</Text>
                     <Counter count={getValues('application_command')?.name?.length} max={32}/>
                 </FormLabel>
-                <input {...register('application_command.name', { required: true, pattern: /^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u, maxLength: 32 }) } id='application_command.name' />
+                <input {...register('application_command.name', { required: true, pattern: /^[-_\p{Ll}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/u, maxLength: 32 }) } id='application_command.name' />
                 <ErrorMessage error={errors?.application_command?.name}/>
                 <FormLabel htmlFor={'application_command.description'} display='flex' alignItems='flex-end'>
                     <Text>Description</Text>

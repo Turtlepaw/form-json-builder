578bbd46842b33abef131121b4a4ff1151453271
Antouto <anthonydsmart@gmail.com>
2023-08-16 11:24:54 +0200

    UI improvements

--- components/JSONViewer.tsx
+++ components/JSONViewer.tsx
@@ -27,7 +27,7 @@ function JSONViewer({ children, downloadForm: downloadFile, getValues }: JSONVie
 
   return (
     <Box style={{ border: `1px solid ${colorMode === 'dark' ? '#292b2f' : '#e3e5e8'}`, borderRadius: '4px', background: colorMode === 'dark' ? '#2f3136' : '#f2f3f5', color: colorMode === 'dark' ? '#e0e1e5' : '#4f5660', width: '100%' }}>
-      <Collapse startingHeight={147} style={{ overflow: 'scroll', backgroundColor: colorMode == "dark" ? "#2f3136" : '#f2f3f5', borderRadius: '4px', padding: '8px' }} in={show}>
+      <Collapse startingHeight={147} style={{ overflow: 'scroll', backgroundColor: colorMode == "dark" ? "#2f3136" : '#f2f3f5', borderRadius: '4px', padding: '8px', fontSize: '14px' }} in={show}>
         <Highlight className='json'>{children}</Highlight>
       </Collapse>
       <Box display='flex' alignItems='center' justifyContent='space-between' padding='4px 12px 4px 4px' borderTop={`1px solid ${colorMode === 'dark' ? '#292b2f' : '#e3e5e8'}`} color='#b8b9bf'>

e118e38bd157f15e22c2745b8a6b40d7d323accb
Antouto <anthonydsmart@gmail.com>
2024-01-27 13:57:26 +0100

    Update Meta.tsx

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,9 +4,9 @@ import Router from "next/router";
 
 export const Configuration = {
     Color: "#5865f2",
-    Title: "Form Builder",
-    TagLine: "Create fully customisable in-app forms",
-    Description: "Create custom in-app modals and send submissions to a channel using webhooks.",
+    Title: "Discord Form Builder",
+    TagLine: "Create fully customisable in-app forms for discord.",
+    Description: "Create custom forms for discord (using in-app modals) and send submissions to a discord channel or google sheets using webhooks.",
     Icon: {
         SVG: "outline.svg",
         Blurple: {
@@ -37,23 +37,24 @@ export function Meta({ children: Title }: MetaProperties) {
             <meta name="title" content={EmbedTitle} />
             <meta name="description" content={Configuration.Description} />
             <meta name="theme-color" content={Configuration.Color} />
-            {/* <meta property="og:image" content={Thumbnail} />
+            <meta property="og:image" content={Thumbnail} />
             <meta property="og:image:type" content="image/png" />
             <meta property="og:image:width" content="250" />
-            <meta property="og:image:height" content="250" /> */}
+            <meta property="og:image:height" content="250" />
 
             {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             <meta property="og:url" content={WebsiteURL} />
             <meta property="og:title" content={EmbedTitle} />
             <meta property="og:description" content={Configuration.Description} />
+            <meta property="og:site_name" content={Configuration.Title} />
 
             {/* Twitter */}
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={WebsiteURL} />
             <meta property="twitter:title" content={EmbedTitle} />
             <meta property="twitter:description" content={Configuration.Description} />
-            {/* <meta property="twitter:image" content={Thumbnail} /> */}
+            <meta property="twitter:image" content={Thumbnail} />
         </Head>
     );
 }

7ed6c208b94efad1b66f95fba35ba11b98472e5e
TurtlePaw <81275769+Turtlepaw@users.noreply.github.com>
2024-03-06 19:26:15 -0500

    Add edge runtime

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,17 @@
-import NextAuth from "next-auth"
-import DiscordProvider from "next-auth/providers/discord"
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
 
+export const runtime = "edge";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     DiscordProvider({
       clientId: process.env.NEXTAUTH_ID as string,
-      clientSecret: process.env.NEXTAUTH_SECRET as string
+      clientSecret: process.env.NEXTAUTH_SECRET as string,
     }),
     // ...add more providers here
   ],
-}
+};
 
 const handler = NextAuth(authOptions) as unknown;
 export { handler as GET, handler as POST };
